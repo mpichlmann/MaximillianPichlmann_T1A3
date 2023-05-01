d6c388511dc736f1492f529dbc1a3dee097d6ab3
mpichlmann <14205@coderacademy.edu.au>
2023-05-02 09:26:35 +1000

    added a for loop for button pushes in room 2

--- room1.py
+++ room1.py
@@ -12,7 +12,6 @@ def starting_room():
 
 def room1():
     print("You are in Room 1.")
-    # Code for actions in Room 1
     move = input("Enter 'back' to go back to the starting room: ")
     if move == "back":
         starting_room()
@@ -22,32 +21,40 @@ def room1():
 
 def room2():
     print("You are in Room 2. In front of you is a button")
-    # Code for actions in Room 2
     button_presses = 0
-    while True:
+    messages = ["You push the button, but nothing happens.", 
+                "You push the button again, but this time you hear a slight clicking noise.",
+                "You push the button for the third time, the clicking noise grows.",
+                "You hear a rumbling sound and a secret door opens!"]
+    for i in range(3):
         move = input("Enter 'back' to go back to the starting room or 'push' to push the button: ")
         if move == "back":
             starting_room()
             return
         elif move == "push":
             button_presses += 1
-            if button_presses >= 5:
-                print("You hear a rumbling sound and a secret door opens!")
-                move = input("do you enter the secret door or head back? type 'enter' or 'back'")
-                if move == "back":
-                    starting_room()
-                elif move == "enter":
-                    secret_room()
-                return
-            else:
-                print("You push the button, but nothing happens.")
+            if button_presses < len(messages)-1:
+                print(messages[button_presses])
+            elif button_presses == len(messages)-1:
+                print(messages[button_presses])
         else:
             print("Invalid choice.")
+            continue
+
+    move = input("do you enter the secret door or head back? type 'enter' or 'back' ")
+    if move == "enter":
+        secret_room()
+    elif move == "back":
+        room2()
+    else:
+        print("Invalid choice.")
+        room2()
+
+
 
 def secret_room():
     print("you are in a secret room")
-    # Code for actions in secret room
-    move = input("'back' to go back 'look' to look around the secret room")
+    move = input("'back' to go back 'look' to look around the secret room ")
     if move == "back":
         room2()
     elif move == "look":
