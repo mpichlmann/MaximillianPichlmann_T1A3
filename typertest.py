eafc882678cc4e281a1771ff8f889db043224e5c
mpichlmann <14205@coderacademy.edu.au>
2023-05-02 09:11:58 +1000

    tested ascii art

--- typertest.py
+++ typertest.py
@@ -6,8 +6,13 @@ def typewriter_text(text):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.01)
     print('')  # add newline after typewriter effect
 
 # Example usage:
-typewriter_text("Hello and welcome to the game, prepare to be spooked")
+typewriter_text(" ( )(_))\ ) `  )   ))\   /(_)) (    (     ))\ )\())\())\  (    )\))(   ")
+typewriter_text("(_(_()|()/( /(/(  /((_) (_))   )\   )\  '/((_|_))((_)((_) )\ )((_))\   ")
+typewriter_text("|_   _|)(_)|(_)_\(_))   / __| ((_)_((_))(_)) | |_| |(_|_)_(_/( (()(_)  ")
+typewriter_text("  | | | || | '_ \) -_)  \__ \/ _ \ '  \() -_)|  _| ' \| | ' \)) _` |   ")
+typewriter_text("  |_|  \_, | .__/\___|  |___/\___/_|_|_|\___| \__|_||_|_|_||_|\__, |   ")
+typewriter_text("       |__/|_|                                                |___/    ")
