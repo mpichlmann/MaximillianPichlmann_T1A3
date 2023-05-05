5da150d8845db598591192486d875315a78aed18
mpichlmann <14205@coderacademy.edu.au>
2023-05-05 11:42:22 +1000

    added car waiting messages

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def game_over():
     game_over_choice = input("Would you like to [play] again? ")
     if game_over_choice == "play":
         starting_room()
+    else: 
+
+def road():
+    print("you are on a road")
 
 def wood_death():
     print("---------------------------------------------------------")
@@ -14,6 +18,34 @@ def wood_death():
     print("you've triggered a makeshift trap, nails fly at you from all directions, killing you")
     game_over()
 
+def car_wait():
+    wait_time = 0
+    wait_messages = ["You wander what exactly it is you're waiting for...", 
+                "You can't really think of anything that you're waiting for...",
+                "You think someone finding you on this quiet road in the middle night is pretty unlikely, but still... you wait.",
+                "resolute in your decision to wait, you start to become more confident in your ability to wait",
+                "You can do this, you can wait all night if it takes",
+                "By sheer wait-power, you will will your car back to life you tell yourself"]
+    print("---------------------------------------------------------")
+    print("you wait in the car, nothing happens")
+    move = input("do you want to [wait] some more, explore the [woods] or walk up the [road] on foot? ")
+    while move == 'wait':
+        if wait_time < len(wait_messages):
+            print(wait_messages[wait_time])
+            wait_time += 1
+        else:
+            print("you start to get PISSED OFF")
+        move = input("do you want to [wait] some more, explore the [woods] or walk up the [road] on foot? ")
+    if move == 'woods':
+        wood_death()
+    elif move == 'road':
+        road()
+    else: 
+        print("Invalid Choice")
+        car_wait()
+        
+
+
 def starting_room():
     print("---------------------------------------------------------")
     print("It's almost midnight, you are driving on a long empty road")
@@ -23,9 +55,9 @@ def starting_room():
     print("Do you.. [wait]? walk up the [road] on foot? or explore the [woods]? ")
     choice = input("Enter your choice: ")
     if choice == "wait":
-        room1()
+        car_wait()
     elif choice == "road":
-        room2()
+        road()
     elif choice == "woods":
         wood_death()
     else:
