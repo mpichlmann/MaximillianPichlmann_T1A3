089d159b3da2d0a4cd04dda24da4963ebc687ff1
mpichlmann <14205@coderacademy.edu.au>
2023-05-05 10:35:23 +1000

    added a start menu

---
+++ main.py
@@ -0,0 +1,9 @@
+play_game = False
+print("Welcome to 'Spooky Woods' an exploration horror puzzle game!")
+print("Your goal is to reach the end and save the day!")
+print("Throughout the game you will be presented with choices")
+print("Your options will be presented in [square] brackets")
+print("simply type your option to proceed, but be careful!")
+ready = input("Ready to [play]? ")
+if ready == "play":
+    play_game = True
--- room.py
+++ room.py
@@ -1,14 +1,25 @@
-print("you are in a room with two doors in front of you, type 1 to enter door 1 and type 2 to enter door 2")
-room = "starting room"
+play_game = False
+print("Welcome to 'Spooky Woods' an exploration horror puzzle game!")
+print("Your goal is to reach the end and save the day!")
+print("Throughout the game you will be presented with choices")
+print("Your options will be presented in [square] brackets")
+print("simply type your option to proceed, but be careful!")
+ready = input("Ready to [play]? ")
+if ready == "play":
+    play_game = True
 
-while True: 
-    door = int(input())
-    if door == 1: 
-        print("you are now in room 1")
-        room = "room 1"
-    elif door == 2: 
-        print("you are now in room 2")
-        room = "room 2"
-    elif door == "go back":
-        print("You are now back in the starting room")
+while play_game is True:
+    print("you are in a room with two doors in front of you, type 1 to enter door 1 and type 2 to enter door 2")
+    room = "starting room"
+
+    while True: 
+        door = int(input())
+        if door == 1: 
+            print("you are now in room 1")
+            room = "room 1"
+        elif door == 2: 
+            print("you are now in room 2")
+            room = "room 2"
+        elif door == "go back":
+            print("You are now back in the starting room")
     
