3c8d27ea8d252b319bc8f9e2c80f650f59aae815
mpichlmann <14205@coderacademy.edu.au>
2023-05-06 10:37:42 +1000

    added conditions for church being quiet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 
-
+church_quiet = False
 signpost_visited = False
 soup_eaten = 0
 
@@ -21,39 +21,67 @@ def game_over():
             game_over_choice = input("Would you like to [play] again? or do you want to [quit] playing? ")
 
 def manor():
-    pass
+    print("---------------------------------------------------------")
+    print("you follow the path to until you reach a double story house")
+    print("The house resembles an old victorian architectural style")
+    print("you notice the door is wide open")
+    print("---------------------------------------------------------")
+    move = input("do you [knock] at the door, simply [open] the door and let yourself in, do you [look] in through a window or head [back] to the signpost? ")
 
-def dorms():
-    pass
-    
+
 
 def church():
     global signpost_visited
     signpost_visited = True
-    print("---------------------------------------------------------")
-    print("You follow the direction the sign pointed until you come across a church")
-    print("inside you can hear music and singing, although now that you're closer it sounds more like chanting")
-    print("you think maybe someone inside can help you fix your car")
+    global church_quiet
+    if church_quiet == False:
+        print("---------------------------------------------------------")
+        print("You follow the direction the sign pointed until you come across a church")
+        print("inside you can hear music and singing, although now that you're closer it sounds more like chanting")
+        print("you think maybe someone inside can help you fix your car")
+        print("---------------------------------------------------------")
+        church_quiet = True
+    else:
+        print("---------------------------------------------------------")
+        print("You follow the direction the sign pointed until you come across a church")
+        print("you can't hear anything now, but you're sure this is where the music was coming from")
+        print("you think maybe someone inside can help you fix your car")
+        print("---------------------------------------------------------")
     move = input("do you [knock] at the door, simply [open] the door and let yourself in, do you [look] in through a window or head [back] to the signpost? ")
     while True:
-        if move == "knock":
+        if move == "knock" and church_quiet == False:
             print("---------------------------------------------------------")
             print("the music and chanting stops, you hear boots marching towards the door")
             print("the door swings open, you are met by a group of people donned in hoods")
             print("'REMOVE THE INTERLOPER' a voice shouts")
             print("the group of hooded figures reveal the knives under their cloaks and launch at you")
             game_over()
+        elif move == 'knock' and church_quiet == True:
+            print("---------------------------------------------------------")
+            print("you hear a high pitched voice scream out for help")
+            print("---------------------------------------------------------")
+            move = input("do you [knock] at the door, simply [open] the door and let yourself in or head [back] to the signpost? ")
         elif move == "open":
             print("---------------------------------------------------------")
             print("You try to the door but can't, the door is locked")
+            print("---------------------------------------------------------")
             move = input("do you [knock] at the door, simply [open] the door and let yourself in, do you [look] in through a window or head [back] to the signpost? ")
-        elif move == 'look': 
+        elif move == 'look' and church_quiet == False:
             print("---------------------------------------------------------")
             print("You peer through the window and see a group of hooded figures in black robes standing in a circle")
             print("In the center of them you see a woman in a mechanics uniform strapped to an upside down cross")
             print("standing opposite the woman is a figure in a red robe, holding a tome in one hand, and an ornate knife in the other")
             print("You think to yourself 'Maybe that woman in the mechanics uniform can help me fix my car!'")
             print("you also think she might be in a bit of trouble and be in need of some rescuing")
+            print("---------------------------------------------------------")
+            move = input("do you [knock] at the door, simply [open] the door and let yourself in or head [back] to the signpost? ")
+        elif move == 'look' and church_quiet == True:
+            print("---------------------------------------------------------")
+            print("You peer through the window and see a group of hooded figures lying on the ground in a circle")
+            print("In the center of all the figures you see a woman in a mechanics uniform strapped to an upside down cross")
+            print("You think to yourself 'Maybe that woman in the mechanics uniform can help me fix my car!'")
+            print("you also think she might be in a bit of trouble and be in need of some rescuing")
+            print("---------------------------------------------------------")
             move = input("do you [knock] at the door, simply [open] the door and let yourself in or head [back] to the signpost? ")
         elif move == 'back':
             signpost()
@@ -77,6 +105,8 @@ def kitchen():
 def mess_hall():
     global signpost_visited
     signpost_visited = True
+    global church_quiet
+    church_quiet = True
     global soup_eaten
     if kitchen_return == False and soup_eaten == 0:
         print("---------------------------------------------------------")
@@ -136,11 +166,9 @@ def signpost():
         print("you return to the signpost")
         print("you notice you can no longer hear the music or the singing that you did previously")
         print("---------------------------------------------------------")
-    move = input("The signpost reads [mess hall],[dorms],[church],[manor]")
+    move = input("The signpost reads [mess hall],[church],[manor]")
     if move == 'mess hall':
         mess_hall()
-    elif move == 'dorms':
-        dorms()
     elif move == 'church':
         church()
     elif move == 'manor':
